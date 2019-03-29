55162f5479516187027b2f6cb8cbaea4f3835b53
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-03-29 11:20:17 -0500

    Used forms. Select to allow the user to select the make of there vehicle.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+CHOICES = (('Ford', 'Ford'), ('Chevy', 'Chevy'))
+
 
 class PostVehicleForm(forms.Form):
     car_img = forms.URLField(
@@ -7,8 +9,22 @@ class PostVehicleForm(forms.Form):
             "class": "form-control mb-3",
             "placeholder": "Image URL"
         }))
-    make = forms.CharField(
+    year = forms.IntegerField(
         widget=forms.TextInput(attrs={
             "class": "form-control mb-3",
-            "placeholder": "Cars Make"
+            "placeholder": "Cars Year"
+        }))
+    make = forms.CharField(
+        widget=forms.Select(
+            choices=CHOICES, attrs={"class": "form-control mb-3"}))
+
+    model = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control mb-3",
+            "placeholder": "Cars Model"
+        }))
+    mileage = forms.IntegerField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control mb-3",
+            "placeholder": "Cars Mileage"
         }))
